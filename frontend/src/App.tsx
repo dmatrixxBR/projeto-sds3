ef9a13c9528ce767da3036a16fd41d8e4bbb66ad
dmatrixx <dmatrixx@gmail.com>
2021-09-08 13:44:04 -0300

    Apex Static Charts Components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
+import BarChart from "components/barchart";
 import DataTable from "components/datatable";
+import DonutChart from "components/donutchart";
 import Footer from "components/footer";
 import NavBar from "components/navbar";
 
@@ -7,9 +9,24 @@ function App() {
     <>
       <NavBar />
       <div className='container'>
-        <h1 className='text-primary'>
-          Olá Mundo
+        <h1 className='text-primary py-3'>
+          DashBoard de Vendas
         </h1>
+        <div className='row px-3'>
+          <div className='col-sm-6'>
+            <h5 className='text-center text-secondary'>Percentual de Sucesso</h5>
+            <BarChart />
+          </div>
+
+          <div className='col-sm-6'>
+            <h5 className='text-center text-secondary'>Total de Vendas</h5>
+            <DonutChart />
+          </div>
+
+        </div>
+        <div className='py-3'>
+          <h2 className='text-primary'>Total de Vendas</h2>
+        </div>
       <DataTable />
       </div>
       <Footer />
