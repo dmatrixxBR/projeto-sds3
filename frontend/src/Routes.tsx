741a1411298f2ad84bf9ca802b6450eeb5d8e5a6
dmatrixx <dmatrixx@gmail.com>
2021-09-10 12:41:30 -0300

    Routes

---
+++ frontend/src/Routes.tsx
@@ -0,0 +1,22 @@
+import DashBoard from 'pages/dashboard';
+import Home from 'pages/home';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+const Routes = () => {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path='/' exact>
+                    <Home />
+                </Route>
+
+                <Route path='/dashboard' exact>
+                    <DashBoard />
+                </Route>
+            </Switch>
+
+        </BrowserRouter>
+
+    );
+}
+
+export default Routes ;
---
+++ frontend/src/pages/home/index.tsx
@@ -0,0 +1,13 @@
+const Home = () => {
+    return (
+
+        <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
+            <div className="container">
+              <h2>Pagina Home</h2>
+            </div>
+        </div>
+
+    );
+}
+
+export default Home;
